831c2b4533ab5372cdac620ae6ac87fe9dfad686
root <root@localhost.localdomain>
2026-09-18 10:55:41 +0000

    fix: add compose-compiler-gradle-plugin to buildscript classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,6 @@ buildscript {
         classpath("com.github.megatronking.stringfog:xor:5.0.0")
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.4.10")
     }
 }
